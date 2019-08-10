1f887ed2b28c2c9956c7171efbd5855a262a554a
leebonghee <s2cronoss@gmail.com>
2019-08-10 21:01:25 +0900

    change

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Toy0801
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ko
+    config.time_zone = 'Seoul'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
