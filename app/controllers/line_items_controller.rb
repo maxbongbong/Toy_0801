c73c24f446fa7a7a051a2e597989a6b5915711e5
leebonghee <s2cronoss@gmail.com>
2019-08-20 17:42:47 +0900

    update

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  before_action :check_current_user
+
   def add_cart
     product = Product.where(id: params[:product_id]).first
     get_cart.line_items.create(product: product, quantity: 1, amount: product.price)
@@ -16,5 +18,9 @@ class LineItemsController < ApplicationController
     line_item = LineItem.where(id: params[:id]).first
     line_item.destroy
     redirect_to cart_orders_path
+  end
+
+  def check_current_user
+    redirect_to new_user_session_path unless current_user
   end
 end
