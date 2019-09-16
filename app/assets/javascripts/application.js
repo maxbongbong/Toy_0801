3638d0711b49f66e4d6b0d039a4394069e2a0c85
leebonghee <s2cronoss@gmail.com>
2019-09-16 15:26:40 +0900

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// = require jquery3
 //= require rails-ujs
+//= require turbolinks
+//= require popper
 //= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require bootstrap-sprockets
 //= require_tree .
